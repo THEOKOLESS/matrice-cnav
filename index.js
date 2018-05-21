72b8f716205259a0b3a2f213327616645d2a75d8
Theophile Vitoux <tvitoux@student.42.fr>
2018-05-21 22:25:38 +0200

    beugged

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
  	$(document).on('click', '.toogle' , function(){
     var $link = $(this);
     $link.parent().parent().next().slideToggle();
-        //console.log($link.children().prop('id')); 
         if ($link.hasClass('visible')) {
             $link.children().prop('value', '+');
     } else {
@@ -16,15 +15,232 @@ $(document).ready(function(){
 	// });
 
 
-	// $( "#startdate" ).change(function() {
-	// 	var date = new Date($("#startdate").val());
-	// 	 //console.log(date);
-	// 	var newDate = date.toString('dd-MM-yy');
-	// 	//console.log(newDate);
-	// 	date.setMonth(date.getDay() + 3);
-	//     $('#enddate').val(date.getDate());
-	// });
+//  steps !!
 
+// var form = $("#contact");
+//   form.validate({
+//       errorPlacement: function errorPlacement(error, element) { element.after(error); },
+//   });
+// form.children("div").steps({
+//     headerTag: "h3",
+//     bodyTag: "section",
+//     transitionEffect: "slideLeft",
+//     labels: 
+//     {
+//         finish: "envoyer",
+//         next: "Etape suivante",
+//         previous: "Etape precedente"
+//     },
+//     onStepChanging: function (event, currentIndex, newIndex)
+//     {
+
+//         form.validate().settings.ignore = ":disabled,:hidden";
+//          // var fv = $('#contact').data('formValidation'), // FormValidation instance
+//          //            // The current step container
+//          //            $container = $('#contact').find('section[data-step="' + currentIndex +'"]');
+
+//          //        // Validate the container
+//          //        fv.validateContainer($container);
+
+//          //        var isValidStep = fv.isValidContainer($container);
+//          //        if (isValidStep === false || isValidStep === null) {
+//          //            // Do not jump to the next step
+//          //            return false;
+//          //        }
+
+//          //        return true;
+        
+//         return form.valid();
+//     },
+//     onFinishing: function (event, currentIndex)
+//     {
+//         form.validate().settings.ignore = ":disabled";
+//         return form.valid();
+//     },
+//     onFinished: function (event, currentIndex)
+//     {
+//         alert("Submitted!");
+//     }
+
+// });
+
+// jQuery.extend(jQuery.validator.messages, {
+//     // required: "vous devez remplire ce champ pour continuer le questionaire",
+//     required: "",
+//     situf: "caca",
+//     remote: "Please fix this field.",
+//     email: "Please enter a valid email address.",
+//     url: "Please enter a valid URL.",
+//     date: "Please enter a valid date.",
+//     dateISO: "Please enter a valid date (ISO).",
+//     number: "Please enter a valid number.",
+//     digits: "Please enter only digits.",
+//     creditcard: "Please enter a valid credit card number.",
+//     equalTo: "Please enter the same value again.",
+//     accept: "Please enter a value with a valid extension.",
+//     maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+//     minlength: jQuery.validator.format("Please enter at least {0} characters."),
+//     rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+//     range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+//     max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+//     min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+// });
+$("#contact").steps({
+        headerTag: "h3",
+        bodyTag: "section",
+        saveState: true,
+       
+        // Triggered when clicking the Previous/Next buttons
+        onStepChanging: function (e, currentIndex, newIndex) {
+            var fv = $('#contact').data('formValidation'), // FormValidation instance
+                // The current step container
+                $container = $('#contact').find('section[data-step="' + currentIndex + '"]');
+
+            // Validate the container
+            fv.validateContainer($container);
+
+            var isValidStep = fv.isValidContainer($container);
+            if (isValidStep === false || isValidStep === null) {
+                // Do not jump to the next step
+                return false;
+            }
+
+            return true;
+        },
+        // Triggered when clicking the Finish button
+        onFinishing: function (e, currentIndex) {
+            var fv = $('#contact').data('formValidation'),
+                $container = $('#contact').find('section[data-step="' + currentIndex + '"]');
+
+            // Validate the last step container
+            fv.validateContainer($container);
+
+            var isValidStep = fv.isValidContainer($container);
+            if (isValidStep === false || isValidStep === null) {
+                return false;
+            }
+
+            return true;
+        },
+        onFinished: function (e, currentIndex) {
+            // Uncomment the following line to submit the form using the defaultSubmit() method
+            $('#contact').formValidation('defaultSubmit');
+
+            // For testing purpose
+            $('#welcomeModal').modal("show");
+        }
+     }).formValidation({
+        excluded: ':disabled',
+        message: 'This value is not valid',
+        container: 'tooltip',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            //last name validation  
+            sd_lastname: {
+                container: 'popover',
+                validators: {
+                    notEmpty: {
+                        message: 'The Last Name is required and cannot be empty'
+                    }
+                }
+            },
+
+            //first name validation
+            sd_firstname: {
+                container: 'popover',
+                validators: {
+                    notEmpty: {
+                        message: 'The First Name is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 3,
+                        max: 30,
+                        message: 'The First Name must be more than 7 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[A-Z]+$/i,
+                        message: 'The First Name can only consist of alphabetical characters'
+                    }
+                }
+            },
+
+            //validation of Parent's details step start
+            //last name validation  
+            pd_lastname: {
+                container: 'popover',
+                validators: {
+                    notEmpty: {
+                        message: 'The Last Name is required and cannot be empty'
+                    }
+                }
+            },
+
+            //first name validation
+            pd_firstname: {
+                container: 'popover',
+                validators: {
+                    notEmpty: {
+                        message: 'The First Name is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 3,
+                        max: 30,
+                        message: 'The First Name must be more than 7 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[A-Z]+$/i,
+                        message: 'The First Name can only consist of alphabetical characters'
+                    }
+                }
+            },
+
+            // Validation for Reference details starts
+            //School reference name
+            rd_schoolrefname: {
+                container: 'popover',
+                validators: {
+                    notEmpty: {
+                        message: 'The School Reference Name is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 7,
+                        max: 40,
+                        message: 'The School Reference Name must be more than 7 and less than 40 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[A-Z\s]+$/i,
+                        message: 'The School Reference Name can only consist of alphabetical characters'
+                    }
+                }
+            },
+            //School reference phone
+            rd_schoolrefmobile: {
+                container: 'popover',
+                validators: {
+                    notEmpty: {
+                        message: 'The Phone or Mobile is required and cannot be empty'
+                    }
+                }
+            },
+            rd_schoolrefemail: {
+                container: 'popover',
+                validators: {
+                    notEmpty: {
+                        message: 'The E-Mail ID is required and cannot be empty'
+                    },
+                    regexp: {
+                        regexp: /[a-zA-Z0-9]+(?:(\.|_)[A-Za-z0-9!#$%&'*+\/=?^`{|}~-]+)*@(?!([a-zA-Z0-9]*\.[a-zA-Z0-9]*\.[a-zA-Z0-9]*\.))(?:[A-Za-z0-9](?:[a-zA-Z0-9-]*[A-Za-z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?/g,
+                        message: 'The E-Mail ID is not a valid E-Mail'
+                    }
+                }
+            },
+        }
+
+    })
     $( function() {
         $( "#famidate" ).datepicker({
             changeMonth: true,
