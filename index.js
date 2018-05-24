863805f3cf4b1703b27a46ee12555c2f18de536d
Theophile VITOUX <tvitoux@e1r6p13.42.fr>
2018-05-24 21:53:47 +0200

    42

--- index.js
+++ index.js
@@ -57,7 +57,18 @@ $(document).ready(function(){
             changeMonth: true,
             changeYear: true,
              minDate: "-100 Y",  
-            maxDate: new Date() 
+            maxDate: new Date(),
+            dateFormat: 'dd/mm/yy' 
+    });
+    });
+
+    $( function() {
+        $( "#conjbrth" ).datepicker({
+            changeMonth: true,
+            changeYear: true,
+             minDate: "-100 Y",  
+            maxDate: new Date(),
+            dateFormat: 'dd/mm/yy' 
     });
     });
 
@@ -96,7 +107,7 @@ $(document).ready(function(){
             var date3mois = new Date($("#startdate").val());
             var month = date3mois.getMonth();
             // if (month > 3){
-              month = month + 3;
+              month = month + 2;
             // }
             date3mois.setMonth(month);
             // date3mois.setUTCDate('32');
@@ -126,7 +137,7 @@ $(document).ready(function(){
 
             var date    = new Date($('#startdate').val());
             var moisfr  = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
-            var month   = date.getMonth() + 1;
+            var month   = date.getMonth();
             var year    = date.getFullYear()
             var index   = 0;
             var output  = null;
@@ -147,20 +158,11 @@ $(document).ready(function(){
         }
 
 
-        function    drop(){
-
-            var status = $('#myDropdown').val();
-
-            console.log(status);
-        }
-
-        drop();
 
 
 
         $(document).on('change', '#myDropdown' , function(){
 
-          console.log($("#myDropdown").val())
           var status = $('#myDropdown').val();
           if (status == "celibataire" || status == "Veuf" || status == "Divorce" || status == "Sépare" || status == "Conjoint disparu")
         {
@@ -172,9 +174,64 @@ $(document).ready(function(){
         }
         });
 
-    function getValue(param) {
+
+        $(document).on('change', '#myDropdown' , function(){
+
+          var status = $('#myDropdown').val();
+          if (status == "celibataire")
+        {
+            $('#conj').hide();
+        }
+        else if (status == "Veuf" || status == "Divorce" || status == "Sépare" || status == "Conjoint disparu")
+        {
+             $('#conj').show();
+             document.getElementById("conj-tittle").innerHTML = "Identité de votre ex-conjoint(e), décédé(e) ou disparu(e)"; 
+        }
+        else
+        {
+             $('#conj').show();
+             document.getElementById("conj-tittle").innerHTML = "Identité de votre conjoint(e) actuel(le)"; 
+        }
+        });
+
+
+//         $("#cnracl").click(function() {
+//     var checked=this.checked;
+//     if(checked==true)
+//         {
+//           console.log("tototototo");
+//          $('#cnracl').show()
+//         }
+//     else
+//         {
+//          $('#cnracl').hide()
+//         }
+// });
+
+
+
+    $(document).on('change', '.retcomp' , function(){
+   if ($(this).is(':checked')) {
+     // getChecked((this).id);
+     getChecked(document.getElementById((this).id));
+      // console.log("tototototo");
+      // $('#cnracl').show(); 
+   } else {
+      // $('#cnracl').hide();
+      // (this).id.show();
+    }
+  
+    // $('#cnracl').slideToggle();
+        });
+
+    function getChecked(param){
+      param.show();
+    }
+
+function getValue(param) {
       var value =  param       // je ferais ma requete
       return value;
     }
+    
 });
 
